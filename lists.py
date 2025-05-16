3aff1ef38bb0b5081e65006e4368b291afb3a180
TomasCarrizoAslanian <tcarrizoaslanian@mail.austral.edu.ar>
2025-05-15 23:11:35 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,20 +1,30 @@
-# Replace the "ANSWER HERE" with your answer
-
 def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
-
+    lista1 = list_to_remove_elements
+    if len(lista1) > 5:
+        del lista1[5]
+    if len(lista1) > 4:
+        del lista1[4]
+    if len(lista1) > 0:
+        del lista1[0]
+    return lista1
 
 def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
-
+    list_to_add_elements.append('Yellow')
+    list_to_add_elements.insert(0,'Pink')
+    return list_to_add_elements
 
 def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
+    return len(list_to_check) == 0
 
 
 def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
-
+    if len(list_to_compare1) >= 3 and len(list_to_compare2) >= 3:
+        return list_to_compare1[2] == list_to_compare2[2]:
+    else:
+        return False
 
 def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+    a = list_of_lists_to_modify[0][:2]
+    b = list_of_lists_to_modify[1][1:4]
+    c = list_of_lists_to_modify[2][-2:]
+    return [a, b, c]
